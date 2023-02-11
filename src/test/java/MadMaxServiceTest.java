39ed1d49d5ffe8055488ce130c3dcf2e3434bd79
Gvynhartt <savshush134@gmail.com>
2023-02-11 04:50:11 +0300

    .yml setup template updated

--- src/test/java/MadMaxServiceTest.java
+++ src/test/java/MadMaxServiceTest.java
@@ -9,10 +9,22 @@ public class MadMaxServiceTest {
         int a = 5;
         int b = 3;
 
-        int expected = 7;
+        int expected = 5;
         int actual = benzak.max(a, b);
 
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void shdFindMaxIfB() {
+        MadMaxService slav = new MadMaxService();
+
+        int a = 3;
+        int b = 5;
+
+        int expected = 5;
+        int actual = slav.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
